8c84885430b21684f08d205854f0ad23fe99a703
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-10-31 12:45:21 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!.</h1>
+      <h1>Todo List</h1>
     </div>
   );
 }
 
 export default App;
+
